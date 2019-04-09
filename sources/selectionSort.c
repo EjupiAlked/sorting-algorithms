cb9cac3911d0d47047ec0bfd8e28a7096fa3ec60
Alked Ejupi <ae7g18@soton.ac.uk>
2019-04-09 13:28:05 +0100

    selectionSort Test

--- sources/selectionSort.c
+++ sources/selectionSort.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include "header.h"
 
+void swap(int *a, int *b) {	// swap the position of the two elements in the array
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 void selectionSort(int A[], int n) {
     int i, k, j;
 
-    for(i = 0, i < n, i++){
-
+    for(i=0, i<(n-1), i++){
+        for(i=k=j, j<n, j++){
+            if(A[j]<A[k]){
+                k = j;
+            }
+        }
+        swap(&A[i],&A[k]);
     }
 }
 
