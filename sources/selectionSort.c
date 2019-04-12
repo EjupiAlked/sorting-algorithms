133cbed7534f17e02d74db1e34ad7ad03c8e93f6
Alked Ejupi <ae7g18@soton.ac.uk>
2019-04-12 08:59:11 +0100

    fix

--- sources/selectionSort.c
+++ sources/selectionSort.c
@@ -9,7 +9,8 @@ void swap(int *a, int *b) {	// swap the position of the two elements in the arra
 void selectionSort(int A[], int n) {
     int i, k, j;
 
-    for(i=0; i<(n-1); i++){
+    fo
+    r(i=0; i<(n-1); i++){
         for(j=k=i; j<n; j++){
             if(A[j]<A[k]){
                 k = j;
